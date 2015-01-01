5847cb22c2b8365f47c205b8d29bc962fd9aa30c
tiagoddinis@hotmail.com <tiagoddinis@hotmail.com>
2015-01-01 12:41:03 +0000

    Incomplete Client - Pushing so jorod makes ServerListener

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,8 +1,111 @@
+
 package client;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Scanner;
+
+
 public class Client {
+    private Socket cSocket;
+    private boolean notLogged;
+    private Scanner input;
+
+
+    public Client(int port) {
+        try{
+            cSocket = new Socket("localhost", 12345);
+        }
+        catch (IOException io) {
+            System.out.println("Ups, IO problem!");
+        }
+
+        input = new Scanner(System.in);
+        notLogged = true;
+    }
+
 
     public static void main(String[] args) {
+        Client client = new Client(12345);
+        client.run();
+    }
 
+    public void run(){
+        String str1 = "nothing yet", str2;
+        int cOption;
+
+        while(notLogged){
+            System.out.println("------Login------");
+            System.out.print("Username: ");
+            str1 = input.nextLine();
+            System.out.print("Password: ");
+            str2 = input.nextLine();
+            tryLogin(str1,str2);
+        }
+
+        System.out.println("\nWelcome " + str1);
+        System.out.println("\n1.Create a new Task Type\n2.Start a Task\n3.Finish a Task\n4.See available Task Types"
+                + "\n5.See running Tasks\n6.Subscribe to Task List\n7.Stock up Warehouse\n8.Log off");
+
+        while( !(str1 = input.nextLine()).equals("8")){
+            System.out.println("\n1.Create a new Task Type\n2.Start a Task\n3.Finish a Task\n4.See available Task Types"
+                    + "\n5.See running Tasks\n6.Subscribe to Task List\n7.Stock up Warehouse\n8.Log off");
+
+            if( (cOption = goodInput(str1)) > 0 ){
+                switch(cOption){
+                    case 1: createTaskType();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    case 9:
+                        break;
+                }
+            }
+            else System.out.println("\nBad Input, but please, do try again.");
+        }
+    }
+
+    public void tryLogin(String un, String pw){
+        notLogged = false;
+    }
+
+    public int goodInput(String str){
+        int res;
+        try{
+            res = Integer.parseInt(str);
+        } catch(NumberFormatException e){
+            return -1;
+        }
+        if(res>0 && res<9)
+            return res;
+        else return -1;
+    }
+
+    public void createTaskType(){
+        String str1, str2, str3;
+        HashMap<String,Integer> needs;
+        System.out.println("\nNew TaskType name: ");
+        str1 = input.nextLine();
+
+        System.out.println("\nNeeded material name: ");
+        while( !(str2 = input.nextLine()).equals("cancel")){
+            System.out.println("\nNeeded material name: ");
+
+        }
+        System.out.println("\nNeeded material name: ");
+        createTaskTypeObject(str1,needs);
     }
 }
