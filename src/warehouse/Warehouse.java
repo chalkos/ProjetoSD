ecef15aef8be15e3c121580234ab15af2e03d471
Fernando Mendes <devfrmendes@gmail.com>
2015-01-04 18:37:54 +0000

    Fixes item invalid quantity deadlock

--- src/warehouse/Warehouse.java
+++ src/warehouse/Warehouse.java
@@ -29,20 +29,20 @@ public class Warehouse {
         stockLock.lock();
         Item i = stock.get(itemName);
 
-
-        if(i == null)
+        if (i == null)
             i = new Item(itemName);
 
-        i.lock();
-
         stockLock.unlock();
-
-        i.add(quantity);
+        i.lock();
+        try {
+            i.add(quantity);
+        } finally {
+            i.unlock();
+        }
 
         stockLock.lock();
         stock.put(itemName, i);
 
-        i.unlock();
         stockLock.unlock();
     }
 
